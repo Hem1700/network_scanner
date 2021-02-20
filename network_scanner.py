184f6381975e4c30e0793cfe78a157d83ca9981d
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-20 18:57:45 +0530

    function print_result implemented

--- network_scanner.py
+++ network_scanner.py
@@ -7,15 +7,22 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list= scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("--------------------------------------------------------------------------------------")
-    print("IP\t\t\tMAC Address")
-    print("--------------------------------------------------------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" +element[1].hwsrc)
-        
+        client_dict = {"ip":element[1].psrc ,"MAC Address":element[1].hwsrc}
+        clients_list.append(client_dict)      
+    return clients_list
     #print(answered_list.summary())
     #print(arp_request.summary())
     #scappy.ls(function name) - lists all the fields which we can set to a function
 
+def print_result(result_list):
+    print("--------------------------------------------------------------------------------------")
+    print("IP\t\t\tMAC Address")
+    print("--------------------------------------------------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["MAC Address"])
 
-scan("10.0.2.1/24")
+
+scan_result=scan("10.0.2.1/24")
+print_result(scan_result)

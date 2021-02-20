cc637ff210adc97a08e223712a928a07db60719a
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-20 14:46:53 +0530

    Analysing Packets

--- network_scanner.py
+++ network_scanner.py
@@ -6,11 +6,14 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
-    print(unanswered.summary())    
+    answered_list= scapy.srp(arp_request_broadcast, timeout=1)[0]
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("---------------------------------------------------------------------------------")
+    #print(answered_list.summary())
     #print(arp_request.summary())
     #scappy.ls(function name) - lists all the fields which we can set to a function
 
 
-scan("10.0.2.1/14")
+scan("10.0.2.1/24")
